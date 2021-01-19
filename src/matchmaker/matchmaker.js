14f628a74e040c4886cf37ae8a65976d0b8305ca
Tom Overland <tomoverland03@gmail.com>
2021-01-18 19:20:02 -0600

    created /port-open/ route and a handlePortOpen function

--- src/matchmaker/matchmaker.js
+++ src/matchmaker/matchmaker.js
@@ -3,7 +3,7 @@ class Matchmaker {
 
   handleJoinQueue(req, res) {
     const respObj = {
-      clientMatcherID: '12345',
+      clientMatcherID: "12345",
     };
     res.json(respObj);
   }
@@ -12,16 +12,16 @@ class Matchmaker {
     const respObj = {
       matchers: [
         {
-          matcherID: '11111',
-          address: '8.8.8.8',
+          matcherID: "11111",
+          address: "8.8.8.8",
         },
         {
-          matcherID: '22222',
-          address: '8.8.8.8',
+          matcherID: "22222",
+          address: "8.8.8.8",
         },
         {
-          matcherID: '33333',
-          address: '8.8.8.8',
+          matcherID: "33333",
+          address: "8.8.8.8",
         },
       ],
     };
@@ -31,7 +31,14 @@ class Matchmaker {
   handlePingResult(req, res) {
     const respObj = {
       shouldStartMatch: true,
-      matcherAddress: '192.168.1.1:12345',
+      matcherAddress: "192.168.1.1:12345",
+    };
+    res.json(respObj);
+  }
+
+  handlePortOpen(req, res) {
+    const respObj = {
+      // Empty obj for now, will need to fill in KVP with more info
     };
     res.json(respObj);
   }
--- src/routes.js
+++ src/routes.js
@@ -1,39 +1,47 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
 class RoutesController {
   init(matchmaker) {
     this.matchmaker = matchmaker;
     this.router = new express.Router();
     this.proxyRoutes();
-    console.log('router init complete', this.matchmaker);
+    console.log("router init complete", this.matchmaker);
     return this.router;
   }
 
   proxyRoutes() {
     this.router.post(
-      '/join-matchmaking-queue/',
+      "/join-matchmaking-queue/",
       bodyParser.json({
-        limit: '1024kb',
-        type: 'application/json',
+        limit: "1024kb",
+        type: "application/json",
       }),
       this.matchmaker.handleJoinQueue
     );
     this.router.post(
-      '/get-matcher-address/',
+      "/get-matcher-address/",
       bodyParser.json({
-        limit: '1024kb',
-        type: 'application/json',
+        limit: "1024kb",
+        type: "application/json",
       }),
       this.matchmaker.handleGetMatchers
     );
     this.router.post(
-      '/ping-result/',
+      "/ping-result/",
       bodyParser.json({
-        limit: '1024kb',
-        type: 'application/json',
+        limit: "1024kb",
+        type: "application/json",
       }),
       this.matchmaker.handlePingResult
+    );
+    this.router.post(
+      "/port-open/",
+      bodyParser.json({
+        limit: "1024kb",
+        type: "application/json",
+      }),
+      this.matchmaker.handlePortOpen
     );
   }
 }
