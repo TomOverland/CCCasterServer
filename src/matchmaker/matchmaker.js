ed9c43704cbdce6434b76971295fd66e5f7f0e87
Peter Anderson <peter.alan.anderson@gmail.com>
2021-01-22 19:52:50 -0600

    finished WSS refactor - have a better idea of what work needs to be done

--- src/matchmaker/matchmaker.js
+++ src/matchmaker/matchmaker.js
@@ -8,7 +8,6 @@ const constants = require('../common/constants/constants');
 class Matchmaker {
   constructor() {
     this.queue = {
-      notLocated: {},
       NW: {}, // North America West
       NE: {}, // North America East
       SA: {}, // Sourth America
@@ -20,9 +19,7 @@ class Matchmaker {
     this.queue;
     this.maxPing = 126;
     this.handleJoinQueue = this.handleJoinQueue.bind(this);
-    this.handleGetMatchers = this.handleGetMatchers.bind(this);
     this.handlePingResult = this.handlePingResult.bind(this);
-    this.deleteMatcher = this.deleteMatcher.bind(this);
     this.handleDumpQueue = this.handleDumpQueue.bind(this);
   }
   createMatcherID() {
@@ -32,7 +29,7 @@ class Matchmaker {
   handleJoinQueue(ws) {
     const matcherID = this.createMatcherID();
     ws.matcherID = matcherID;
-    this.queue.notLocated[matcherID] = ws;
+    // this.queue.notLocated[matcherID] = ws;
 
     console.log(`NEW MATCHER - matcherID is ${ws.matcherID}`);
     const respObj = {
@@ -48,7 +45,12 @@ class Matchmaker {
       console.log('is geolocation response');
       this.handleGeolocationResponse(parsedMessage, ws);
     } else {
-      // Has Match work goes here
+      const opponent = this.queue[regionQueue][clientMatcherID].isMatchedWith;
+      if (opponent) {
+        console.log('IN HANDLE PING RESULT - PLAYER HAS BEEN CLAIMED');
+        console.log('TERMINATING');
+        return; // player has been marked by opponent - that thread will start the match
+      }
       // Ping Comparison Function work goes here
     }
     const respObj = {
@@ -60,47 +62,11 @@ class Matchmaker {
     ws.send(JSON.stringify(respObj));
   }
 
-  handleGetMatchers(req, res) {
-    // Has Match goes here
-    const respObj = {
-      matchers: [
-        {
-          matcherID: '11111',
-          address: '8.8.8.8',
-        },
-        {
-          matcherID: '22222',
-          address: '8.8.8.8',
-        },
-        {
-          matcherID: '33333',
-          address: '8.8.8.8',
-        },
-      ],
-    };
-    res.json(respObj);
-  }
-
   handlePortOpen(req, res) {
     const respObj = {
       // if port status is "true", it is open.
       portStatus: true,
     };
-    res.json(respObj);
-  }
-
-  isMatchedWith(clientMatcherID, res) {
-    const regionQueue = clientMatcherID.substring(0, 2);
-    const opponentMatcherID = this.queue[regionQueue][clientMatcherID].isMatchedWith;
-    if (typeof opponentMatcherID === 'undefined') {
-      return;
-    }
-    const respObj = {
-      shouldStartMatch: true,
-      matchAddress: this.queue[regionQueue][opponentMatcherID].address,
-      matchPort: this.queue[regionQueue][opponentMatcherID].port,
-    };
-
     res.json(respObj);
   }
 
@@ -111,12 +77,8 @@ class Matchmaker {
   }
 
   handleGeolocationResponse(parsedMessage, ws) {
-    const closestRegionCode = this.findClosestRegion(parsedMessage.matchers);
-    const newMatcherID = `${closestRegionCode}-${ws.matcherID}`;
-    const clientMatcher = _.clone(this.queue.notLocated[ws.matcherID]);
-    clientMatcher.matcherID = newMatcherID;
-    delete this.queue.notLocated[ws.matcherID];
-    this.queue[closestRegionCode][newMatcherID] = clientMatcher;
+    ws.regionCode = this.findClosestRegion(parsedMessage.matchers);
+    this.queue[ws.regionCode][ws.matcherID] = ws;
   }
 
   isGeolocationResponse(message) {
